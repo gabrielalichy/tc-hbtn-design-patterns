e55227a71c2e6838dd6502bdf34b7410c6381ef7
Gabriela Lichy <105449193+gabrielalichy@users.noreply.github.com>
2022-07-21 14:43:44 -0300

    naves

---
+++ 0x03/naves/CanhaoExtraDecorator.java
@@ -0,0 +1,13 @@
+public class CanhaoExtraDecorator extends NaveEspacialDecorator {
+
+
+    public CanhaoExtraDecorator(NaveEspacial naveASerDecorada) {
+        super(naveASerDecorada);
+    }
+
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 75;
+    }
+
+}
---
+++ 0x03/naves/LaserDecorator.java
@@ -0,0 +1,13 @@
+
+
+public class LaserDecorator extends NaveEspacialDecorator {
+
+    public LaserDecorator(NaveEspacial naveASerDecorada) {
+        super(naveASerDecorada);
+    }
+
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 40;
+    }
+}
